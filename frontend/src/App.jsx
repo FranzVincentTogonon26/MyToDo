b4b65f2bb6cddc95008a3f59589ed833050c00b0
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-10 21:13:10 +0800

    Building Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import { useAuth } from "./context/useAuth"
+
 
 function App() {
 
+  const { loading, isAuthenticated } = useAuth();
+
+  if(loading){
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="animate-spin" />
+      </div>
+    )
+  }
 
   return (
     <div>Hello</div>
--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { AuthContext } from './AuthContext';
 
-const AuthProvider = () => {
-  return (
-    <div>AuthProvider</div>
-  )
+export const AuthProvider = ({children}) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthStatus = () => {
+        try {
+            const token = localStorage.getItem('token');
+            const user = localStorage.getItem('user');
+
+            if(token && user){
+                setUser(JSON.parse(user));
+                setIsAuthenticated(true);
+            }
+
+        } catch (error) {
+            console.error(`Authentication check failed:`, error);
+            logout();
+        } finally {
+            setLoading(false)
+        }
+    };
+
+    checkAuthStatus();
+
+  }, []);
+
+  const login = ( user, token ) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', user)
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  const value = {
+    user,
+    loading,
+    isAuthenticated,
+    login,
+    logout
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+
 }
-
-export default AuthProvider
