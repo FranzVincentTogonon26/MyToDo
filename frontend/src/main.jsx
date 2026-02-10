37510ca96a0a6822d3d2656bce1b31e325b86c5e
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-10 20:49:37 +0800

    Building FrontEnd

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './context/AuthProvider.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <Toaster position='top-center' toastOptions={{duration:3000}} />
+      <App />
+    </AuthProvider>
+  </StrictMode>,
+)
